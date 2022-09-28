e24391a4fd72c320a1bc0f129d5371734b9da481
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-28 20:21:38 -0300

    animation for header ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,56 @@
 import React from "react";
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 
-import { Feather } from '@expo/vector-icons'
-import { MotiView, MotiText} from 'moti'
+import { Feather } from "@expo/vector-icons";
+import { MotiView, MotiText } from "moti";
 
 const statusBarHeight = StatusBar.currentHeight
   ? StatusBar.currentHeight + 22
   : 64;
 
-export default function Header({name}) {
+export default function Header({ name }) {
   return (
     <View style={styles.container}>
       <MotiView
-      from={{
-        translateY: -150,
-        opacity: 0,
-      }}
-      animate={{
-        translateY: 0,
-        opacity: 1,
-      }}
-      transition={{
-        type: 'timing',
-        duration: 800,
-        delay: 300
-      }}
-      style={styles.content}>
-        <Text style={styles.username}>{name}</Text>
+        from={{
+          translateY: -150,
+          opacity: 0,
+        }}
+        animate={{
+          translateY: 0,
+          opacity: 1,
+        }}
+        transition={{
+          type: "timing",
+          duration: 800,
+          delay: 300,
+        }}
+        style={styles.content}
+      >
+        <MotiText
+          from={{
+            translateX: -300,
+          }}
+          animate={{
+            translateX: 0,
+          }}
+          transition={{
+            type: "timing",
+            duration: 800,
+            delay: 800,
+          }}
+          style={styles.username}
+        >
+          {name}
+        </MotiText>
         <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
-            <Feather name="user" size={27} color="#FFF"/>
+          <Feather name="user" size={27} color="#FFF" />
         </TouchableOpacity>
       </MotiView>
     </View>
@@ -39,28 +61,28 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#8000ff",
     paddingTop: statusBarHeight,
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingStart: 16,
     paddingEnd: 16,
-    paddingBottom: 44
+    paddingBottom: 44,
   },
   content: {
     flex: 1,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   username: {
     fontSize: 28,
-    color: '#FFF',
-    fontWeight: 'bold'
+    color: "#FFF",
+    fontWeight: "bold",
   },
   buttonUser: {
     width: 44,
     height: 44,
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 44 / 2
-  }
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 44 / 2,
+  },
 });
